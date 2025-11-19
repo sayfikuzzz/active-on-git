755a5c34bccd6270accac9a06fa5ebfa90789bd2
sayfikuzzz <artem.sayfutdinov.07@mail.ru>
2025-11-19 23:38:25 +0500

    Refactor generate_password function formatting
    Fix indentation and clean up whitespace in password generation function.

--- aa.py
+++ aa.py
@@ -5,7 +5,7 @@ def generate_password():
     """Базовая функция генерации пароля"""
     print("Добро пожаловать в генератор паролей!")
     length = int(input("Введите длину пароля: "))
-    
+
     # Пока просто генерируем пароль из букв
     characters = string.ascii_letters
     password = ''.join(random.choice(characters) for _ in range(length))

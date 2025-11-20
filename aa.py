3267393897b466ff3340afa1b8cb284482b3f56d
sayfikuzzz <artem.sayfutdinov.07@mail.ru>
2025-11-20 23:56:16 +0500

    Update aa.py
    vxbnmfgc

--- aa.py
+++ aa.py
@@ -3,6 +3,17 @@ import string
 import re
 from datetime import datetime
 
+def generate_password():
+    """Базовая функция генерации пароля"""
+    print("Добро пожаловать в генератор паролей!")
+    length = int(input("Введите длину пароля: "))
+
+    # Пока просто генерируем пароль из букв
+    characters = string.ascii_letters
+    password = ''.join(random.choice(characters) for _ in range(length))
+    
+    return password
+
 
 if __name__ == "__main__":
     password = generate_password()

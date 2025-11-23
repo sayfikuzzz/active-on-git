c525abc6930041e689766707eadf6c6ff8d2744e
sayfikuzzz <artem.sayfutdinov.07@mail.ru>
2025-11-23 23:33:33 +0500

    Update aa.py
    xvxhfdhdh

--- aa.py
+++ aa.py
@@ -7,7 +7,9 @@ def generate_password():
     """Базовая функция генерации пароля"""
     print("Добро пожаловать в генератор паролей!")
     length = int(input("Введите длину пароля: "))
-
+    # Пока просто генерируем пароль из букв
+    characters = string.ascii_letters
+    password = ''.join(random.choice(characters) for _ in range(length))
     
     return password
 

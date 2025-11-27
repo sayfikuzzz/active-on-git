5cf8221795febb2ad2de333f5e21e46cf5a405c5
sayfikuzzz <artem.sayfutdinov.07@mail.ru>
2025-11-27 21:19:34 +0500

    Update aa.py

--- aa.py
+++ aa.py
@@ -113,6 +113,8 @@ def generate_password():
     if not characters:
         print("Ошибка: нужно выбрать хотя бы один тип символов!")
         return None
+
+
     
     password = ''.join(random.choice(characters) for _ in range(length))
     
